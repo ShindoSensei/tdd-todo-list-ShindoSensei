392c660ba212d503a8935123261516c4c1428c38
Justin Teo <justin.tsw@gmail.com>
2017-02-06 21:15:23 +0800

    Todo functions and assertion test completed. Bonus not done

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -2,39 +2,93 @@ const uuidGenerator = require('uuid/v4')
 const fs = require('fs')
 
 const todos = []
-//// the following line will instead load the todos from a json file when the app starts
+// // the following line will instead load the todos from a json file when the app starts
 // const todos = require('../data.json')
 
-//// The following function can be used to save the todos array to the json data file
+// // The following function can be used to save the todos array to the json data file
 // function save () {
 //   const json = JSON.stringify(todos)
 //   fs.writeFileSync('data.json', json, 'utf8')
 // }
 
 // CREATE - params should be an object with keys for name, description and completed
+
 function create (params) {
+  if (params.hasOwnProperty('name') && params.hasOwnProperty('description') && params.hasOwnProperty('completed') && params.name.length >= 5) {
+    params['_id'] = uuidGenerator()
+    todos.push(params)
+    return true
+  } else {
+    return false
+  }
 }
 
 // READ (list & show)
+  // return array of all todos
 function list () {
-  // return list of all TODOs
+  return todos
 }
+
+// find the TODO with this id
+// Should return the Todo Object with the specified id
+// Should return null if no TODO with that id exists
 function show (id) {
-  // find the TODO with this id
+  for (var i = 0; i < todos.length; i++) {
+    if (todos[i]['_id'] === id) {
+      return todos[i]
+    }
+  }
+  return null
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
-function update (id, params) {
+// Should be able to update the Todo with the given id, using the following KVPs (Key-Value Pairs) in the updatedParams object:
+// name
+// description
+// completed (true/false)
+// Should allow individual fields to be updated
+// Should NOT allow a name to be changed to blank or less than 5 characters in length
+// Should return true if an update is successful, false if otherwise
+function update (id, updatedParams) {
+  if (updatedParams.name.length < 5) {
+    return false
+  }
+  for (var i = 0; i < todos.length; i++) {
+    if (todos[i]['_id'] === id) {
+      todos[i].name = updatedParams.name
+      todos[i].description = updatedParams.description
+      todos[i].completed = updatedParams.completed
+      return true
+    }
+  }
+  return false
 }
 
-// DESTROY (destroy & destroyAll)
+// Should be able to delete the Todo with the given id
+// Should return true if the delete is successful, false if otherwise
 function destroy (id) {
+  for (var i = 0; i < todos.length; i++) {
+    if (todos[i]['_id'] === id) {
+      todos.splice(i, 1)
+      return true
+    }
+  }
+  return false
 }
+// [0,1,2,3,4]
 
+// Should be able to delete all the Todos and return true
+function destroyAll () {
+  for (var i = todos.length; i >= 0; i--) {
+    todos.pop()
+  }
+  return true
+}
 module.exports = {
   create,
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -1,5 +1,72 @@
 const assert = require('assert')
 const todos = require('../controllers/todos_controller.js')
 
-// // Use Assert to Test the functionality of all your CRUD methods e.g.
+// Use Assert to Test the functionality of all your CRUD methods e.g.
 // assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+
+// Write assert test to see if create(params) does the following,
+// Should be able to create a new Todo with the following KVPs (Key-Value Pairs) in the params object:
+// name
+// description
+// completed (true/false)
+// Should automatically create an _id property for each new TODO and assign it a UUID
+// Should be able to create a new Todo with just name, sensible defaults will be used for other fields.
+// Should NOT be able to create a new Todo without a name being provided
+// Should NOT allow a name less than 5 characters long
+
+var params = {
+  name: 'names',
+  description: 'Do it',
+  completed: false
+}
+
+var noName = {
+  description: 'Do it',
+  completed: false
+}
+
+var shortName = {
+  name: 'name',
+  description: 'Do it',
+  completed: false
+
+}
+
+// Testing Create function
+assert.strictEqual(todos.create(params), true, 'Create should return an object when run')
+
+assert.strictEqual(todos.create(noName), false, 'Parameter object needs to have name property')
+
+assert.strictEqual(todos.create(shortName), false, 'Parameter object name needs to be at least 5 characters long')
+
+// Testing list function
+assert.ok(todos.list().length > 0, 'List should return an array of all todos')
+
+// Test show function
+
+assert.deepStrictEqual(todos.show(todos.list()[0]['_id']), params, 'Show function should return the Todo Object with the specified id')
+
+// Test update function
+
+var updatedObj = {
+  name: 'newName',
+  description: 'Do not do it',
+  completed: false
+}
+
+var firstObjId = todos.list()[0]['_id']
+
+assert.strictEqual(todos.update(firstObjId, updatedObj), true, 'Object with unique ID is not updated'
+)
+
+assert.strictEqual(todos.update(firstObjId, shortName), false, 'Updated parameter name value should be at least 5 characters long.'
+)
+
+// Test destroy function
+assert.strictEqual(todos.destroy(firstObjId), true, 'Todo with given id not destroyed')
+
+assert.strictEqual(todos.destroy(123), false, 'Todo with given id not destroyed')
+
+// Test destroyAll() function
+todos.destroyAll()
+assert.ok(todos.list().length === 0, 'Destroy All should have no object in todos')
